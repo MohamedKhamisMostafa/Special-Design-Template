950ab6b9bc3016572cec71d129b3c108952139fa
Mohamed.Kh Elhwary <MohamedKhamisMostafa@gmail.com>
2023-11-23 07:01:24 +0200

    Settings Box js code

--- js/master.js
+++ js/master.js
@@ -1,3 +1,13 @@
+// Toggle Spin Class On Icon
+document.querySelector(".toggle-settings .fa-gear").onclick = function () {
+  // Toggle Class Fa-spin for Rotation on Self
+  this.classList.toggle('fa-spin')
+  // Toggle Class Open on Main Settings Box
+  document.querySelector('.settings-box').classList.toggle('open')
+}
+
+
+
 // Select Landing Page Element
 let landingPage = document.querySelector('.landing-page');
 
