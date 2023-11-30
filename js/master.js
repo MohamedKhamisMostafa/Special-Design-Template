6cd9d4b47aec90cdf95c2b1c6db361332e5676db
Mohamed.Kh Elhwary <MohamedKhamisMostafa@gmail.com>
2023-11-30 20:39:41 +0200

    animate skills When reaching the section

--- js/master.js
+++ js/master.js
@@ -156,3 +156,31 @@ function randomaizImgs() {
   }
 }
 randomaizImgs();
+
+  let ourSkills = document.querySelector(".skills");
+
+  window.onscroll = function () {
+
+    // Skills Offset Top
+    let skillsOffsetTop = ourSkills.offsetTop;
+
+    // Skills Outer Height
+    let skillsOuterHeight = ourSkills.offsetHeight;
+    
+    // Window Height
+    let windowHeight = this.innerHeight;
+
+    // Window ScrollTop
+    let windowScrollTop =this.scrollY;
+    
+    if  (windowScrollTop > (skillsOffsetTop + skillsOuterHeight - windowHeight )-100) {
+
+      let allSkills = document.querySelectorAll('.skill-box .skill-progress span')
+
+      allSkills.forEach(skill => {
+        skill.style.width = skill.dataset.progress;
+
+      })
+      
+    }
+  }
