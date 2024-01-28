2a6a455a8f645f7de596f82e4a4d3a77eddcc539
Mohamed.Kh Elhwary <MohamedKhamisMostafa@gmail.com>
2024-01-28 02:23:09 +0200

    Finalize Menu And Logic

--- js/master.js
+++ js/master.js
@@ -393,3 +393,48 @@ document.querySelector(".reset-options").onclick = function () {
   //Reload Window
   window.location.reload();
 }
+
+// Toggle Menu 
+let toggleBtn = document.querySelector(".toggle-menu");
+let tLinks = document.querySelector(".links");
+
+toggleBtn.onclick = function (e) {
+
+  // Stop Propagation
+  e.stopPropagation();
+
+  // Toggle Class "menu-active" On Button
+  this.classList.toggle("menu-active");
+
+  // Toggle Class "open " On Links
+  tLinks.classList.toggle("open");
+
+}
+
+// Click Anywhere Outside Menu And Toggle Button
+document.addEventListener("click" , (e) => {
+
+  if (e.target !== toggleBtn && e.target !== tLinks ) {
+
+    if (tLinks.classList.contains("open")) {
+      
+      // Toggle Class "menu-active" On Button
+      toggleBtn.classList.toggle("menu-active");
+
+      // Toggle Class "open " On Links
+      tLinks.classList.toggle("open");
+    }
+
+
+
+    
+  }
+
+});
+
+// Stop Propagation On Menu 
+tLinks.onclick = function (e) {
+
+  e.stopPropagation();
+  
+}
